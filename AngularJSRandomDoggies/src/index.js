9a06bb2cc845997e04a9584d754d0382aef7f3d6
13DeAnda <nora.e.de.anda@gmail.com>
2022-08-21 17:26:49 -0500

    angular boilerplate

---
+++ AngularJSRandomDoggies/src/index.js
@@ -0,0 +1,10 @@
+angular.module("scopeExample", []).controller("MyController", [
+  "$scope",
+  ($scope) => {
+    $scope.username = "World";
+
+    $scope.sayHello = () => {
+      $scope.greeting = `Hello ${$scope.username}!`;
+    };
+  },
+]);
